ec530feb0c4169889c2685dcfaaa70d2974d743e
MrFatra <ramadhanfatra2004@gmail.com>
2025-02-15 12:27:00 +0700

    P2D-17: Add FAQ Section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "../components";
-import { About, Article, Header, Schedule, Statistic } from "../sections";
+import { About, Article, FAQ, Header, Schedule, Statistic } from "../sections";
 
 const Home = () => {
   return (
@@ -10,6 +10,7 @@ const Home = () => {
       <Schedule />
       <Article />
       <Statistic />
+      <FAQ />
     </div>
   );
 };
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from "./Header";
 export { default as Schedule } from "./Schedule";
 export { default as Article } from "./Article";
 export { default as Statistic } from "./Statistic";
+export { default as FAQ } from "./FAQ";
